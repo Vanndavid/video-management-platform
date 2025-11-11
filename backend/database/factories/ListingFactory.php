679bea40a9edfe693bf1f0d3ead76bbe6d5b91a6
vanndavid <vanndavidteng@gmail.com>
2025-11-11 12:46:03 +1100

    update faker syntax

--- backend/database/factories/ListingFactory.php
+++ backend/database/factories/ListingFactory.php
@@ -4,11 +4,13 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-class ListingFactory extends Factory {
-    public function definition(): array {
+class ListingFactory extends Factory
+{
+    public function definition(): array
+    {
         return [
-            'title' => $this->faker->streetAddress(),
-            'address' => $this->faker->address(),
+            'title' => fake()->streetAddress(),
+            'address' => fake()->city(),
         ];
     }
 }
